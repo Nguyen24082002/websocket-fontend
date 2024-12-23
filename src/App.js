e06e54938873aa3f5bb6a0db11094afff3a657ee
xnguyen <nb24082002@gmail.com>
2024-12-23 21:34:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,17 +4,38 @@ import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDxTVJsw6hs4In6TqR5xFQ156Drt1QlcXw",
+  authDomain: "websocket-chatapp-1c459.firebaseapp.com",
+  projectId: "websocket-chatapp-1c459",
+  storageBucket: "websocket-chatapp-1c459.firebasestorage.app",
+  messagingSenderId: "333793000397",
+  appId: "1:333793000397:web:af7232ce9c8ffdb3a0a284",
+  measurementId: "G-LN1367W10H",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Router basename="websocket-fontend">
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/setAvatar" element={<SetAvatar />} />
-          <Route path="/" element={<Chat />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -7,6 +7,7 @@ import { allUsersRoute, host } from "../utils/APIRoutes";
 import ChatContainer from "../components/ChatContainer";
 import Contacts from "../components/Contacts";
 import Welcome from "../components/Welcome";
+import Logout from "../components/Logout";
 
 export default function Chat() {
   const navigate = useNavigate();
@@ -48,6 +49,9 @@ export default function Chat() {
   return (
     <>
       <Container>
+        <div className="logout-container">
+          <Logout />
+        </div>
         <div className="container">
           <Contacts contacts={contacts} changeChat={handleChatChange} />
           {currentChat === undefined ? (
@@ -61,15 +65,46 @@ export default function Chat() {
   );
 }
 
+
+// const Container = styled.div`
+//   height: 100vh;
+//   width: 100vw;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   gap: 1rem;
+//   align-items: center;
+//   background-color: #131324;
+//   .container {
+//     height: 85vh;
+//     width: 85vw;
+//     background-color: #00000076;
+//     display: grid;
+//     grid-template-columns: 25% 75%;
+//     @media screen and (min-width: 720px) and (max-width: 1080px) {
+//       grid-template-columns: 35% 65%;
+//     }
+//   }
+// `;
+
+
 const Container = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: 1rem;
   align-items: center;
   background-color: #131324;
+  position: relative;
+
+  .logout-container {
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    z-index: 10;
+  }
+
   .container {
     height: 85vh;
     width: 85vw;
