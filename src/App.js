0242915d4edcd234339d3a4d7acdf383cc53a6f8
xnguyen <nb24082002@gmail.com>
2024-12-23 19:07:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
@@ -7,12 +7,14 @@ import Register from "./pages/Register";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/setAvatar" element={<SetAvatar />} />
-        <Route path="/" element={<Chat />} />
-      </Routes>
+      <Router basename="websocket-fontend">
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/setAvatar" element={<SetAvatar />} />
+          <Route path="/" element={<Chat />} />
+        </Routes>
+      </Router>
     </BrowserRouter>
   );
 }
